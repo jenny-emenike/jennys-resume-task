412ff91821547605919de3caffd88454cf6cbd1e
jenny-emenike <emenikejenny2001@gmail.com>
2021-08-21 22:02:40 +0100

    Added index file and composer.json

---
+++ name.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name= "Jenny Emenike";
+    echo "<h1><b>Nice to meet you everyone.</b>My name is {$name}</h1>";
+    
+    ?>
+</body>
+</html>
---
+++ success.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="success.css">
+    <title>Jenny's Resume</title>
+    <div class="success-box">
+        <img src="pic/tick.jpg" id="tick">
+
+    </div>
+    <p id="success">Submitted successfully</p>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="checkbox">
+
+        </div>
+    </div>
+
+</body>
+
+</html>
